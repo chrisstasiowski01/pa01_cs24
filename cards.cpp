de3e085e58dba74e946a87ff502daf6bd07fbe23
chrisstasiowski01 <49623072+chrisstasiowski01@users.noreply.github.com>
2020-05-05 12:28:36 -0700

    add files

---
+++ cards.cpp
@@ -0,0 +1,130 @@
+//cards.cpp
+//Authors: Chris Stasiowski
+//Implementation of the classes defined in cards.h
+#include "cards.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+// CARDLIST CLASS
+
+// constructor sets up empty list
+
+// creates list of cards given a .txt file name
+void CardList::createHand(string filename){
+  ifstream file;
+  string line;
+  file.open(filename);
+  
+  // checks if file exists
+  if(!file){
+    cerr << "File does not exist." << endl;
+    return;
+  }
+
+  while(1){
+    getline(file, line);
+    if(!file){
+      break;
+    }
+    append(line);
+  }
+  file.close();
+  return;
+}
+
+// cycles through card list and deletes every node
+CardList::~CardList() {
+    Card* next = first;
+    while(next){
+        first = first->next;
+        delete next;
+        next = first;
+    }
+}
+
+// cycles through card list and checks if desired card is in list
+bool CardList::contains(string search) const{
+  Card* p = first;
+  while(p){
+    if(p->cardType == search){
+      return true;
+    }
+    p = p->next;
+  }
+  return false;
+}
+
+void CardList::erase(string del){
+  if (contains(del) == false){
+    return;
+  }
+  Card* p = first;
+  if(p->cardType == del){
+    first = p->next;
+    delete p;
+    return;
+  }
+  while(p){
+    if(p->next->cardType == del){
+      Card* temp = p->next;
+      p->next = temp->next;
+      delete temp;
+      return;
+    }
+    p = p->next;
+  }
+}
+
+void CardList::append(string card) {
+    if (first == 0) { // empty list
+        
+        first = new Card;
+        first->cardType = card;
+        first->next = 0;
+
+    }
+    else {
+        Card *n = first;
+        while (n->next) // not last node yet
+            n = n->next;
+        n->next = new Card;
+        n->next->cardType = card;
+        n->next->next = 0;
+    }
+}
+
+void CardList::print() const {
+    Card *n = first;
+    cout << '[';
+    while (n) {
+        cout << n->cardType;
+        if (n->next)
+            cout << ", ";
+        n = n->next;
+    }
+    cout << ']';
+}
+ostream& operator<<(ostream &output, const CardList& c){
+  Card* n = c.first;
+  while(n){
+    cout << n->cardType << endl;
+    n = n->next;
+  }
+}
+// PLAYER CLASS
+
+// consrtructor (gives name to player)
+Player::Player(string n, CardList& h){
+  name = n;
+  hand = h;
+}
+
+// prints each card in hand on new line
+void Player::printHand(){
+  cout << name << "'s cards:" << endl;
+  cout << &hand;
+}
+
+
+
---
+++ cards.h
@@ -0,0 +1,73 @@
+//cards.h
+//Authors: Chris Stasiowski
+//All class declarations go here
+#include <iostream>
+using namespace std;
+#ifndef CARDS_H
+#define CARDS_H
+/*
+class Card{
+
+  public:
+
+    Card(); // constructor
+    void print() const; // prints card
+    friend bool operator==(const Card& card1, const Card& card2); // compares two cards
+    ~Card(); // destructor
+
+  private:
+    string cardType;
+    Card* next;
+};
+*/
+
+struct Card{
+  string cardType;
+  Card* next;
+};
+
+class CardList{
+
+    public:
+      CardList() : first(0) {}// constructor
+      ~CardList(); // destructor
+      bool contains(string search) const; // returns true if hand contains desired card
+      void erase(string del); // erases desired value from list
+      void append(string card); // appends desired card to end of list
+      void createHand(string filename); // creates hand from input text file
+      void print() const; //prints hand
+      friend ostream& operator<<(ostream& output, const CardList& c);
+      Card* first;
+      
+    private:
+      
+
+
+
+};
+
+class Player{
+  
+  public:
+    Player(string n, CardList& h); // constructor
+    ~Player(); // destructor
+    void printHand(); // prints each card in hand on a new line
+  
+  private:
+    string name;
+    CardList hand;
+};
+
+#endif
+
+// Use this file to define all your classes and public functions
+// You should design your program with OOP prinicples using classes
+// An example of class design would be having Card, CardList and Player as Classes.
+// Card represent Node in your LinkedList which has some data in it
+// CardList can be your LinkedList Class which has operations such as append, remove etc
+// Player class represents your game player
+// Think about the public functions that should be part of each class. (All public function need to be tested in unit testing)
+// One of the important functions is search / find which is essential for players to find cars in their opponent's "hand"
+// Make sure you overload the desired operators
+// This is not the fixed design. We are not expecting exactly 3 classes. You can do with less or more.
+// Important thing to consider is abstraction.
